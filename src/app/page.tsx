3c549386102c170e71c9a36446f0239be5e122c0
Akshay P S <psakshay210@gmail.com>
2025-11-18 17:51:30 +0000

    that didnt happen the other pages background are still gray

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Header />
       <main className="flex-1">
         <Hero />
-        <div>
+        <div className="bg-transparent">
           <Events />
           <Timeline />
           <Sponsors />
