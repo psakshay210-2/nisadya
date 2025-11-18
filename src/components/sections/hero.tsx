c8e0b5ccf3572892d3c65ea9531f99f8bb0db3f5
Akshay P S <psakshay210@gmail.com>
2025-11-18 17:31:35 +0000

    now lets have a bit of gadient

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -7,7 +7,8 @@ export function Hero() {
     <section
       className="relative w-full bg-cover bg-center"
       style={{
-        backgroundImage: "url('/background.png')",
+        backgroundImage:
+          "linear-gradient(to top, hsl(var(--background)) 5%, transparent 50%), url('/background.png')",
       }}
     >
       <div className="container relative z-10 flex min-h-screen flex-col items-center justify-center pt-16 text-center">
