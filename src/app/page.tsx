5a155632f4efceb3215b3f3fdabad39ef0875129
Akshay P S <psakshay210@gmail.com>
2025-12-05 20:57:11 +0000

    its still the same size double its size please

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
                 <Image
                 src="/logo.png"
                 alt="Nisadya Logo"
-                width={500}
-                height={250}
-                className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] w-full h-auto'}
+                width={1200}
+                height={600}
+                className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto'}
                 priority
                 />
             </div>
