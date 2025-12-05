ec905db3655bd60b774ca2be757b9b051c34413c
Akshay P S <psakshay210@gmail.com>
2025-12-05 21:22:20 +0000

    dets devide the Heo into two divs, first div will contain the the logo t

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Footer } from '@/components/layout/footer';
 import { Accommodation } from '@/components/sections/accommodation';
-import { Hero } from '@/components/sections/hero';
+import { HeroTabs } from '@/components/sections/hero-tabs';
 import { InstagramFeed } from '@/components/sections/instagram-feed';
 import Image from 'next/image';
 
@@ -8,18 +8,20 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
-        <section className="flex flex-col items-center justify-start text-center overflow-hidden">
-          <div className="relative -mt-16 md:-mt-32 lg:-mt-64">
+        <section className="flex h-screen flex-col overflow-hidden">
+          <div className="flex h-[30vh] items-center justify-center">
             <Image
               src="/logo.png"
               alt="Nisadya Logo"
               width={1600}
               height={800}
-              className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto max-w-[24rem] md:max-w-2xl lg:max-w-4xl'}
+              className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto max-w-xs sm:max-w-sm md:max-w-2xl lg:max-w-4xl'}
               priority
             />
-            <div className="-mt-16 md:-mt-24 lg:-mt-32">
-                <Hero />
+          </div>
+          <div className="flex h-[70vh] items-start justify-center">
+            <div className="container flex w-full max-w-6xl flex-1 flex-col items-center justify-center">
+              <HeroTabs />
             </div>
           </div>
         </section>
