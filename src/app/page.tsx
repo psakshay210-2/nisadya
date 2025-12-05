3261215b7b86ebe7099c13fd7c21c72cbf5e1eac
Akshay P S <psakshay210@gmail.com>
2025-12-05 21:02:57 +0000

    now get it to absolute top of the the website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,18 +8,18 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
-        <section className="flex flex-col items-center justify-center text-center py-12">
-            <div className="relative animate-fade-in-up">
-                <Image
-                src="/logo.png"
-                alt="Nisadya Logo"
-                width={1200}
-                height={600}
-                className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto'}
-                priority
-                />
-            </div>
-            <Hero />
+        <section className="flex flex-col items-center justify-center text-center pt-8">
+          <div className="relative animate-fade-in-up">
+            <Image
+              src="/logo.png"
+              alt="Nisadya Logo"
+              width={1200}
+              height={600}
+              className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto'}
+              priority
+            />
+          </div>
+          <Hero />
         </section>
         <Accommodation />
         <InstagramFeed />
