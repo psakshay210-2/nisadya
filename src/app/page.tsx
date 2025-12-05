93414ea760d910ba4ff85d9b76fdd83c35577f6f
Akshay P S <psakshay210@gmail.com>
2025-12-05 20:55:24 +0000

    litttle bit more towards the top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Footer } from '@/components/layout/footer';
 import { Accommodation } from '@/components/sections/accommodation';
 import { Hero } from '@/components/sections/hero';
-import { Events } from '@/components/sections/events';
 import { InstagramFeed } from '@/components/sections/instagram-feed';
 import Image from 'next/image';
 
@@ -10,7 +9,7 @@ export default function Home() {
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
         <section className="flex flex-col items-center justify-center text-center py-12">
-            <div className="relative mb-4 animate-fade-in-up">
+            <div className="relative animate-fade-in-up">
                 <Image
                 src="/logo.png"
                 alt="Nisadya Logo"
