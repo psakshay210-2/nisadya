2dc492ba933192d175c481d3dc34a22545222f92
Akshay P S <psakshay210@gmail.com>
2025-12-05 21:17:41 +0000

    now lets put the componet containing events schedule sponsors etc direct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,17 +9,19 @@ export default function Home() {
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
         <section className="flex flex-col items-center justify-start text-center overflow-hidden">
-          <div className="relative -mt-24 md:-mt-48 lg:-mt-64">
+          <div className="relative -mt-24 md:-mt-48 lg:-mt-72">
             <Image
               src="/logo.png"
               alt="Nisadya Logo"
               width={1600}
               height={800}
-              className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto max-w-md md:max-w-2xl lg:max-w-4xl'}
+              className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto max-w-[24rem] md:max-w-2xl lg:max-w-4xl'}
               priority
             />
+            <div className="-mt-8 md:-mt-16">
+                <Hero />
+            </div>
           </div>
-          <Hero />
         </section>
         <Accommodation />
         <InstagramFeed />
