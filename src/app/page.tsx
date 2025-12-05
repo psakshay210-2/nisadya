c018c4155d80bc16cf21af9fd0750c4ca456e7c0
Akshay P S <psakshay210@gmail.com>
2025-12-05 21:07:17 +0000

    its not been happening can you push the image twice the distance from wh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
-        <section className="flex flex-col items-center text-center">
-          <div className="relative animate-fade-in-up -mb-16">
+        <section className="flex flex-col items-center text-center overflow-hidden">
+          <div className="relative animate-fade-in-up -mt-32">
             <Image
               src="/logo.png"
               alt="Nisadya Logo"
-              width={2400}
-              height={1200}
+              width={1600}
+              height={800}
               className={'invert drop-shadow-[0_4px_10px_rgba(0,0,0,1)] h-auto'}
               priority
             />
